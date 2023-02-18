2c913eabf8bf158ba3a4dbcaf5cab6a448f2591c
guhans6 <guhans61101@gmail.com>
2023-02-18 13:24:40 +0530

    LLok adfsdfdsf

---
+++ CRM Project/Assembler.swift
@@ -0,0 +1,25 @@
+//
+//  Assembler.swift
+//  CRM C
+//
+//  Created by guhan-pt6208 on 17/02/23.
+//
+
+import Foundation
+
+
+class Assembler {
+    
+    static func getSplashView() {
+        
+    }
+    
+    
+    
+    static func getModuleTableView() {
+        
+        let networkController = NetworkController()
+//        let modulesPresenter = ModulesPresenter(networkController: networkController)
+        let modulesViewController = ModuleTableViewController()
+    }
+}
